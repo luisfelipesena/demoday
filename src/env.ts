030e5bcb38531086ebbdcf920c4681466cedc2eb
Luis Felipe Sena <luisfelipesena@gmail.com>
2025-04-03 09:43:39 -0300

    chore: update DATABASE_URL to use hardcoded connection string

--- src/env.ts
+++ src/env.ts
@@ -1,5 +1,5 @@
 export const env = {
-    DATABASE_URL: process.env.DATABASE_URL ?? "",
+    DATABASE_URL:  "postgresql://demoday_owner:npg_1jKrTW4DuNGk@ep-cool-bonus-a5rsh1k5-pooler.us-east-2.aws.neon.tech/demoday?sslmode=require",
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET ?? "",
     NEXTAUTH_URL: process.env.NEXTAUTH_URL ?? "http://localhost:3000",
 };
