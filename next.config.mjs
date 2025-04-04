1aaefc341d7e2f59959fd64a7bed84366891b463
Luis Felipe Sena <luisfelipesena@gmail.com>
2025-04-03 22:04:40 -0300

    chore: update DATABASE_URL to use environment variable

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
   output: "standalone",
   reactStrictMode: true,
   env: {
-    DATABASE_URL: "postgresql://demoday_owner:npg_1jKrTW4DuNGk@ep-cool-bonus-a5rsh1k5-pooler.us-east-2.aws.neon.tech/demoday?sslmode=require",
+    DATABASE_URL: process.env.DATABASE_URL,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
   },
